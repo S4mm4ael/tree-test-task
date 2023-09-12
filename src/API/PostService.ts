2adb5c8adcd943af20fe981478a4bca72748b8cb
SEMION KRAPIVIN <homer1996@gmail.com>
2023-09-12 13:44:50 +0200

    feat: add error handling for gettree

--- src/API/PostService.ts
+++ src/API/PostService.ts
@@ -5,13 +5,15 @@ export default class PostService {
 
   static async getTree(treeName: string) {
     const treeUrl = this.baseUrl + "api.user.tree.get";
-
-    const response = await axios.get(treeUrl, {
-      params: {
-        treeName: treeName,
-      },
-    });
-
-    return response.data;
+    try {
+      const response = await axios.get(treeUrl, {
+        params: {
+          treeName: treeName,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
